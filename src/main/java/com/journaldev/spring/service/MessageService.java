d8fffc5de2ec7181693dd00cda9f5d0f0c84f86c
Didem <geceyarisi@live.com>
2017-07-28 14:39:01 +0300

    Update MessageService.java 2

--- src/main/java/com/journaldev/spring/service/MessageService.java
+++ src/main/java/com/journaldev/spring/service/MessageService.java
@@ -1,9 +1,8 @@
 package com.journaldev.spring.service;
 
+import com.journaldev.dao.MessageDAO;
 import com.journaldev.hibernate.data.Message;
 import java.util.List;
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
@@ -11,28 +10,33 @@ import org.springframework.transaction.annotation.Transactional;
 
 @Component
 public class MessageService {
-	@Autowired
-	private SessionFactory sessionFactory;
-
-	public SessionFactory getSessionFactory() {
-		return sessionFactory;
-	}
-
-	public void setSessionFactory(SessionFactory sessionFactory) {
-		this.sessionFactory = sessionFactory;
-	}
 	
-	@Transactional
-	public void register(Message message){
-		// Acquire session
-		Session session = sessionFactory.getCurrentSession();
-		// Save message, saving behavior get done in a transactional manner
-		session.save(message);		
-	}
+    @Autowired
+    private MessageDAO messageDAO;
+
+    public MessageDAO getMessageDAO() {
+        return messageDAO;
+    }
+
+    public void setMessageDAO(MessageDAO messageDAO) {
+        this.messageDAO = messageDAO;
+    }
         
-        @Transactional
-        public List<Message> getAllMessages(){
-            return (List<Message>)sessionFactory.getCurrentSession().createQuery("from Message").list();
-        }
+    @Transactional
+    public void register(Message message){
+        messageDAO.register(message);
+    }
+        
+    @Transactional
+    public List<Message> getPaginatedMessages(){
+        return messageDAO.getPaginatedMessages();
+            
+    }
+
+
+    public void delete(Integer id) {
+        messageDAO.delete(id);
+            
+    }
 
 }
