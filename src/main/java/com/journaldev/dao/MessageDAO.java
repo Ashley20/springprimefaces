db53d4c7dc272d63eb811288b0e95bf359f25f5f
Didem <geceyarisi@live.com>
2017-08-01 10:09:13 +0300

    Update MessageDAO.java

--- src/main/java/com/journaldev/dao/MessageDAO.java
+++ src/main/java/com/journaldev/dao/MessageDAO.java
@@ -7,9 +7,11 @@ package com.journaldev.dao;
 
 import com.journaldev.hibernate.data.Message;
 import java.util.List;
+import org.hibernate.HibernateException;
 import org.hibernate.Query;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
@@ -22,8 +24,9 @@ import org.springframework.transaction.annotation.Transactional;
  */
 
 @Component
-public class MessageDAO implements IMessageDAO {
+public class MessageDAO {
     
+   
     @Autowired
     private SessionFactory sessionFactory;
 
@@ -37,20 +40,38 @@ public class MessageDAO implements IMessageDAO {
     
     
     @Transactional
-    @Override
-    public List<Message> getPaginatedMessages() {
-       return (List<Message>)sessionFactory.getCurrentSession().createQuery("from Message").list();
+    public List<Message> getPaginatedMessages(int first, int pageSize) {
+        
+        List<Message> messages = null;
+      
+        Session session = sessionFactory.openSession();
+        try {
+            session.beginTransaction();
+            Query query = session.createQuery("from Message");
+           
+              query.setFirstResult(first);
+              query.setMaxResults(pageSize);
+              System.out.println(first + "Bu first " + pageSize + " buda pagesize");
+              messages = (List<Message>) query.list();
+              session.getTransaction().commit();
+              
+      
+   
+        } catch (HibernateException e) {
+            e.printStackTrace();
+        } finally {
+            session.close();
+        }
+        return messages;   
     }
 
     @Transactional
-    @Override
     public void register(Message message) {
         sessionFactory.getCurrentSession().save(message);
     }
 
    
     @Transactional
-    @Override
     public void delete(Integer id) {
         Session session = sessionFactory.getCurrentSession();
         Query query = session.createQuery("delete Message where messageId=:ID");
@@ -59,5 +80,19 @@ public class MessageDAO implements IMessageDAO {
         int result = query.executeUpdate();
         
     }
+    
+    @Transactional
+    public int getCount() {
+        int count = ((Long) sessionFactory.getCurrentSession()
+                .createQuery("select count(*) from Message").uniqueResult())
+                .intValue();
+        System.out.println(" count size " + count);
+        return count;
+    }
+    
+    
+    
+     
+
     
 }
