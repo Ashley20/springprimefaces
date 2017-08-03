d64868fa4a114018c223a84f2a8ea425858d1420
Didem <geceyarisi@live.com>
2017-08-03 10:20:32 +0300

    Update Message.java

--- src/main/java/com/journaldev/hibernate/data/Message.java
+++ src/main/java/com/journaldev/hibernate/data/Message.java
@@ -1,10 +1,31 @@
 package com.journaldev.hibernate.data;
 
-public class Message {
+import com.journaldev.listeners.MessageListener;
+import java.io.Serializable;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+
+import javax.persistence.EntityListeners;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.PostRemove;
+import javax.persistence.PrePersist;
+import javax.persistence.PreRemove;
+import javax.persistence.Table;
+
+
+
+@Table(name = "messages")
+@Entity
+public class Message implements Serializable  {
     
     private Integer messageId;
+    
     private String subject;
+    
     private String content;
+    
+    private String read;
     
     public Message(){}
 
@@ -12,10 +33,13 @@ public class Message {
         this.messageId = messageId;
         this.subject = subject;
         this.content = content;
+        this.read = "NO";
     }
     
     
-
+    @Id
+    @GeneratedValue
+    @Column(name = "MESSAGE_ID")
     public Integer getMessageId() {
         return messageId;
     }
@@ -24,6 +48,7 @@ public class Message {
         this.messageId = meassageId;
     }
 
+    @Column(name = "SUBJECT")
     public String getSubject() {
         return subject;
     }
@@ -32,6 +57,7 @@ public class Message {
         this.subject = subject;
     }
 
+    @Column(name = "CONTENT")
     public String getContent() {
         return content;
     }
@@ -39,8 +65,17 @@ public class Message {
     public void setContent(String content) {
         this.content = content;
     }
-    
-    
-	
 
+    @Column(name = "MESSAGE_READ")
+    public String getRead() {
+        return read;
+    }
+
+    public void setRead(String read) {
+        this.read = read;
+    }
+    
+    
+    
+    
 }
