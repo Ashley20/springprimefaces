b76a2c359632ca9eb3f142cd6d5f286373786976
Didem <geceyarisi@live.com>
2017-08-08 08:13:00 +0300

    Update LazySorter.java

--- src/main/java/com/journaldev/prime/faces/beans/LazySorter.java
+++ src/main/java/com/journaldev/prime/faces/beans/LazySorter.java
@@ -6,6 +6,7 @@
 package com.journaldev.prime.faces.beans;
 
 import com.journaldev.hibernate.data.Message;
+import java.lang.reflect.Field;
 import java.util.Comparator;
 import org.primefaces.model.SortOrder;
 
@@ -27,9 +28,14 @@ public class LazySorter implements Comparator<Message> {
     @Override
     public int compare(Message message1, Message message2) {
         try {
-            Object value1 = Message.class.getField(this.sortField).get(message1);
-            Object value2 = Message.class.getField(this.sortField).get(message2);
- 
+            Field field1 = message1.getClass().getDeclaredField(this.sortField);
+            Field field2 = message2.getClass().getDeclaredField(this.sortField);
+            field1.setAccessible(true);
+            field2.setAccessible(true);
+            
+            Object value1 = field1.get(message1);
+            Object value2 = field2.get(message2);
+           
             int value = ((Comparable)value1).compareTo(value2);
              
             return SortOrder.ASCENDING.equals(sortOrder) ? value : -1 * value;
